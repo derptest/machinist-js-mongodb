fe5da31b982fec01ca04400500876a4bb373c773
Warner Onstine <warner@clipppr.com>
2014-03-18 22:37:50 -0700

    adding promises fixed the test problem

--- test/mongodb.test.js
+++ test/mongodb.test.js
@@ -25,16 +25,28 @@ describe("Machinist API", function(){
 
 
     it("it should create a new user using blueprint", function(done){
-        this.timeout(10000);
-        machinist.blueprint("user").make({
+        this.timeout(20000);
+        var promise = machinist.blueprint("user").make({
             name: "Warner Onstine",
             username: "warner",
             emails: ["warner@test.com"]
-        }, function(err, doc){
-            should.not.exist(err);
-            should.exist(doc);
-
         });
+
+        promise.then(
+            function(docs) {
+                // console.log("saved doc %j", docs);
+                should.exist(docs);
+                docs[0].name.should.equal("Warner Onstine");
+
+                //check default value of blueprint
+                docs[0].active.should.equal(true);
+                done();
+            },
+            function(err){
+                should.not.exist(err);
+                done();
+            }
+        );
         
         
     });
